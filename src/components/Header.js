a301480159f194e3ef44de79ff0e1e9b4d1dcd19
Donato Spinelli <donspi@hotmail.it>
2021-06-09 18:24:15 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,12 +19,6 @@ const Header = () => {
           <li>
             <Link to="/about/">About</Link>
           </li>
-          <li>
-            <a href={companyUrl}>
-              <span className="visually-hidden">Github</span>
-              <FaGithub />
-            </a>
-          </li>
         </ul>
       </Container>
     </header>
