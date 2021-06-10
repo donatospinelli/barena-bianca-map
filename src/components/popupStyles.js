f22d260eb29f87bce97f432c3493f2c5009c53dd
Donato Spinelli <donspi@hotmail.it>
2021-06-10 23:31:24 +0200

    customizing popup

---
+++ src/components/popupStyles.js
@@ -0,0 +1,17 @@
+const popupContent = {
+  textAlign: "center",
+ overflow:"scroll",
+  height: "350px",
+  marginTop: "30px"
+};
+const popupHead = {
+  fontWeight: "bold",
+  fontSize: "22px"
+};
+
+const popupText = {
+  fontSize: "15px",
+  marginBottom: "20px"
+};
+
+export { popupContent, popupHead, popupText };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Marker, useMap, Popup } from "react-leaflet";
+import { popupContent, popupHead, popupText } from "components/popupStyles";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
@@ -15,7 +16,7 @@ const LOCATION = {
   lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
-const DEFAULT_ZOOM = 2;
+const DEFAULT_ZOOM = 2.5;
 
 /**
 const popupContentGatsby = `
@@ -34,6 +35,7 @@ const popupContentGatsby = `
  * MapEffect
  * @description This is an example of creating an effect used to zoom in and set a popup on load
  */
+
 
 const MapEffect = ({ markerRef }) => {
   const map = useMap();
@@ -59,13 +61,25 @@ const IndexPage = () => {
 
       <Map {...mapSettings}>
         {letters.map(letter => {
-          const {id, location, title, text } = letter;
+          const {id, location, title, text} = letter;
           const {latitude, longitude} = location;
           const position = [latitude, longitude];
 
             return(
               <Marker key={id} position={position}>
-            <Popup> {title} {text} </Popup>
+            <Popup className="request-popup"> 
+
+            <div style={popupContent}> 
+            <div style={popupHead}> {title} </div>
+             <img
+              src="https://www.cheapflights.com/news/wp-content/uploads/sites/136/2019/04/landscape1.jpg"
+              width="500"
+              height="320"
+              alt="no img"
+            />
+            <span style={popupText}> {text} </span>
+            </div>
+            </Popup>
               </Marker>
             )
           })}
